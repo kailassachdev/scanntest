e2856e888b74a29092d90f2f3146aa0e11d34961
Kailas Sachdev <kailas0sachdev@gmail.com>
2025-06-19 06:31:48 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <CardContent class

--- src/components/results-client-page.tsx
+++ src/components/results-client-page.tsx
@@ -89,7 +89,7 @@ export default function ResultsClientPage() {
           <Separator />
 
           <h3 className="text-xl font-semibold">Answer Breakdown:</h3>
-          <ScrollArea className="max-h-[40vh] sm:max-h-[55vh] md:max-h-[65vh] w-full">
+          <ScrollArea className="max-h-[320px] sm:max-h-[450px] md:max-h-[60vh] w-full">
             <Accordion type="single" collapsible className="w-full">
               {questions.map((q, index) => {
                 const userAnswer = userAnswers[index];
